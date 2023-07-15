bbc816da6e69d7b3ace8ad49eba86f7452fee499
asherlie <lieber.asher@gmail.com>
2023-07-14 22:14:27 -0400

    separated header out of struct ashdrop_msg

--- ashdropd.c
+++ ashdropd.c
@@ -10,11 +10,26 @@ struct ashdrop_hdr{
     uint16_t chunksz;
     uint16_t offset;
     uint16_t totalbytes;
+};
+
+struct ashdrop_msg{
+    struct ashdrop_hdr hdr;
     // variable size data buffer, NULL for requests
     uint8_t* buffer;
 };
 
+struct ashdrop_entry{
+    struct ashdrop_hdr file_inf;
+    /* sender must be noted because file_inf.file_id
+     * is only unique per sender
+     */
+    char* from, * fn;
+    uint16_t file_id;
+};
+
 void* recv_msg_th(void* arg){
+    (void)arg;
+    return NULL;
 }
 
 int main(){
